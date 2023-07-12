4b1362b1b237b2a4bc60139b9a37864e8fe6776a
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2023-07-11 21:36:09 -0500

    end

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="/estilos/style.css">
-    <link rel="stylesheet" type="text/css" href="/estilos/estilospaypal.css">
+    <title>Pago en Linea</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+
+    <!-- SweetAlert -->
+    <link rel="stylesheet" href="sweetalert2.min.css">
+
+    <link rel="stylesheet" type="text/css" href="estilos/estilospaypal.css">
     
 </head>
 <body>
@@ -39,18 +45,18 @@
                 01 / 2018
                 </div>
                 <div class="card-holder">
-                John Doe
+                Pago en Linea
                 </div>
             </div>
             </div>
         </div>
         
         <div class="form">
-            <form action="" method="post">
+            <form  action="https://www.paypal.com/cgi-bin/webscr" method="post">
             <label for="cardholder">Nombre</label>
-            <input type="text" id="cardholder" placeholder="Nombre del titular">
+            <input type="text" id="nombre" placeholder="Nombre del titular" name="cardholder" oninput="validarNombre(this)">
             <label for="cardholder">Correo</label>
-            <input type="text" id="cardholder" placeholder="xxxx@ejemplo.com">
+            <input type="text" id="correo" placeholder="xxxx@ejemplo.com">
             <label for="cardholder">Monto a Pagar</label>
             <input type="number" id="monto-pagar" placeholder="$">
             
@@ -72,4 +78,6 @@
     </div>
 </body>
     <script src="scrip.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="sweetalert2.min.js"></script>
 </html>
--- scrip.js
+++ scrip.js
@@ -2,6 +2,7 @@ paypal.Buttons({
     // Set up the transaction
     createOrder: function(data, actions) {
         var monto = document.getElementById('monto-pagar').value;
+        
         return actions.order.create({
             purchase_units: [{
                 amount: {
@@ -10,11 +11,65 @@ paypal.Buttons({
             }]
         });
     },
+    
     // Finalize the transaction
     onApprove: function(data, actions) {
         return actions.order.capture().then(function(details) {
             // Show a success message to the buyer
-            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+            var to = document.getElementById('correo').value;
+            var monto = document.getElementById('monto-pagar').value;
+            var nombre = document.getElementById('nombre').value;
+            var subject = 'Transicion completa';
+            
+            var body = 'Tu pago ha sido realizado. <b>'+nombre+'</b> <br><br>' +
+                'El monto realizado fue $' + monto + ' USD.<br><br>' +
+                '<div style="background-color: #f2f2f2; padding: 10px;">' +
+                '<strong> Has completado tu pago exitosamente.</strong></div><br><br>' +
+                'Muchas Gracias,<br>';
+            
+            $.ajax({
+                url: 'completed.php',
+                method: 'POST',
+                data: {to: to, subject: subject, body: body},
+                success: function(response) {
+                    // ...
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                    alert('Error al enviar el correo electrónico.');
+                }
+            });
+            
+            
+            Swal.fire('Pago completado con éxito', '¡Gracias por tu compra!', 'success');
+            location.reload();
         });
     }
 }).render('#paypal-button-container');
+
+
+// Validacion nombre
+
+function validarNombre(input) {
+    var nombre = input.value;
+    var pattern = /^[a-zA-Z\s]+$/;  // Expresión regular para letras y espacios
+  
+    // Verificar si el campo está vacío o si contiene solo espacios en blanco // 
+    if (nombre.trim() === '') {
+      return;
+    }
+  
+    if (!pattern.test(nombre)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Por favor, ingresa solo letras en el nombre.'
+      }).then(function() {
+        input.value = '';
+      });
+    }
+  }
+  
+  
+  
+  
